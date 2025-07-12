7a6ac969ecef1b622df2c47c2ec9226f528549a8
Quick_Learner <genesisprognosis1156@gmai.com>
2025-07-12 09:49:16 -0400

    added more stuff

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pytest
-
+import os
 from pages.gear import GearPage
 from pages.sale_page import SalePage
 from pages.login_page import LoginPage
@@ -11,10 +11,16 @@ from pages.login_page import LoginPage
 @pytest.fixture()
 def driver():
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    service = Service() # closes Webdriver
+    # disables a browser feature that tells websites "I'm being controlled by automation"
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    # Pretends to be a normal browser used by a human
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+    prefs = {"download.default_directory": f"{os.getcwd()}/FileDownloads"}
+    options.add_experimental_option("prefs", prefs)
+    service = Service() # closes webdriver
     driver = webdriver.Chrome(service=service, options=options)
     # driver.implicitly_wait(5)
     yield driver
--- pages/gear.py
+++ pages/gear.py
@@ -10,32 +10,31 @@ class GearPage(BasePage):
         shopping_category_element = (By.XPATH, f"//li[@class='item']/a[contains(@href, '/gear/{shopping_category.lower()}.html')]")
         self.find(shopping_category_element).click()
 
-    # WAY 2
-    # category_links = {
-    #     "bags": (By.XPATH, "//li[@class='item']/a[contains(@href, '/gear/bags.html')]"),
-    #     "fitness-equipment": (By.XPATH, "//li[@class='item']/a[contains(@href, '/gear/fitness-equipment.html')]"),
-    #     "watches": (By.XPATH, "//li[@class='item']/a[contains(@href, '/gear/watches.html')]")
-    # }
-    #
-    # def click_shopping_category(self, shopping_category: str):
-    #     locator = self.category_links.get(shopping_category.lower())
-    #     if locator:
-    #         self.find(locator).click()
-    #     else:
-    #         raise ValueError(f"Unknown shopping category: '{shopping_category}'")
+    category_links = {
+        "bags": (By.XPATH, "//li[@class='item']/a[contains(@href, '/gear/bags.html')]"),
+        "fitness-equipment": (By.XPATH, "//li[@class='item']/a[contains(@href, '/gear/fitness-equipment.html')]"),
+        "watches": (By.XPATH, "//li[@class='item']/a[contains(@href, '/gear/watches.html')]")
+    }
 
-    # WAY 3
-    # bags = (By.XPATH, "(//li[@class='item']/a[@href='https://magento.softwaretestingboard.com/gear/bags.html'])[1]")
-    # fitnessEquipment = (By.XPATH, "(//li[@class='item']/a[@href='https://magento.softwaretestingboard.com/gear/fitness-equipment.html'])[1]")
-    # watches = (By.XPATH, "(//li[@class='item']/a[@href='https://magento.softwaretestingboard.com/gear/watches.html'])[1]")
-    #
-    # def click_shopping_category(self, shopping_category: str):
-    #     match shopping_category.lower():
-    #         case "bags":
-    #             self.find(self.bags)
-    #         case "fitness-equipment":
-    #             self.find(self.fitnessEquipment)
-    #         case "watches":
-    #             self.find(self.watches)
-    #         case _:
-    #             print("Not a valid shopping category")
+    def click_shopping_category2(self, shopping_category: str):
+        locator = self.category_links.get(shopping_category.lower())
+        if locator:
+            self.find(locator).click()
+        else:
+            raise ValueError(f"Unknown shopping category: '{shopping_category}'")
+
+
+    bags = (By.XPATH, "(//li[@class='item']/a[@href='https://magento.softwaretestingboard.com/gear/bags.html'])[1]")
+    fitnessEquipment = (By.XPATH, "(//li[@class='item']/a[@href='https://magento.softwaretestingboard.com/gear/fitness-equipment.html'])[1]")
+    watches = (By.XPATH, "(//li[@class='item']/a[@href='https://magento.softwaretestingboard.com/gear/watches.html'])[1]")
+
+    def click_shopping_category3(self, shopping_category: str):
+        match shopping_category.lower():
+            case "bags":
+                self.find(self.bags)
+            case "fitness-equipment":
+                self.find(self.fitnessEquipment)
+            case "watches":
+                self.find(self.watches)
+            case _:
+                print("Not a valid shopping category")
