74e514bf607a12ace065dfc16c8e4c237c794c2e
Quick_Learner <genesisprognosis1156@gmai.com>
2025-07-22 19:27:44 -0400

    added screenshot on fail to be attached to allure report

--- conftest.py
+++ conftest.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -7,25 +9,40 @@ from pages.login_page import LoginPage
 from dotenv import load_dotenv
 import os
 import pytest
+import allure
 
 
 @pytest.fixture()
-def driver():
+def driver(request):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    # disables a browser feature that tells websites "I'm being controlled by automation"
     options.add_argument("--disable-blink-features=AutomationControlled")
-    # Pretends to be a normal browser used by a human
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
     prefs = {"download.default_directory": f"{os.getcwd()}/FileDownloads"}
     options.add_experimental_option("prefs", prefs)
-    service = Service() # closes webdriver
+    service = Service()
     driver = webdriver.Chrome(service=service, options=options)
-    # driver.implicitly_wait(5)
     yield driver
-    driver.quit() # closes browser
+    # Access the test result and attach a screenshot on fail
+    if request.node.rep_call.failed:
+        screenshot = driver.get_screenshot_as_png()
+        allure.attach(screenshot,
+            name=f"Failure Screenshot {datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}",
+            attachment_type=allure.attachment_type.PNG)
+    driver.quit()
+
+
+# Register test outcome to be used in the fixture
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # Execute all other hooks to get the report object
+    outcome = yield
+    rep = outcome.get_result()
+    # Set a report attribute for each phase of a call (setup, call, teardown)
+    setattr(item, f"rep_{rep.when}", rep)
+
 
 @pytest.fixture()
 def login_page(driver):
@@ -51,3 +68,4 @@ def valid_sensitive_creds():
     }
 
 # to run the tests: pytest -m temp_test -s or pytest -m-s
+# to run with allure: 1)rm -rf allure-results 2) pytest --alluredir=allure-results 3) allure serve allure-results
--- tests/test_gear_page.py
+++ tests/test_gear_page.py
@@ -7,10 +7,12 @@ import utils.wait_helpers
 @BasePage.log_time
 @allure.title("Check shopping category opens correct url and Verify specific watch name")
 @allure.severity(allure.severity_level.MINOR)
-@pytest.mark.temp_test
+@pytest.mark.additional_test
 def test_clicking_shopping_category(gear_page):
     gear_page.open_page()
     gear_page.click_shopping_category("watches")
     utils.wait_helpers.wait_for_element_visibility(gear_page._driver, gear_page._header_title_loc)
     assert gear_page.current_url == "https://magento.softwaretestingboard.com/gear/watches.html"
     gear_page.verify_specific_watch_name('bolo sport watch')
+
+
